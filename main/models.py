1c972e8326894ffad538e24e066dcaa6edcdf539
Jonstance <54332180+Jonstance@users.noreply.github.com>
2021-04-15 19:38:35 +0100

    just

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class Books(models.Model):
     bookDetails = models.CharField(max_length=1000)
     bookAmount = models.IntegerField(default=0)
     bookLink = models.URLField(blank=True)
-    bookImage = models.FileField(default="")
+    bookImage = models.ImageField(upload_to="uploaded/")
 
 
 class Blog(models.Model):
